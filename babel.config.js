f790d2b295ccd90322e6a7868915f2781524c66f
DJCai <2230968394@qq.com>
2018-09-11 20:02:42 +0800

    项目架构构建

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  //iview的按需加载的配置
+  "plugins": [["import", {
+      "libraryName": "iview",
+      "libraryDirectory": "src/components"
+    }]]
+  
 }
